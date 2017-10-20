c6211b2ba05477936cbe55fa76e89def55a14405
chenweihong <15094846015@163.com>
2017-10-20 09:40:12 +0800

    config eslint

--- src/components/firstComponent.js
+++ src/components/firstComponent.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import { Component } from 'react'
 
 class FirstComponent extends Component {
-    render() {
+	render() {
         
-        /*可以在 render 方法里使用 JavaScript 的语法，使用时加上 {} 即可*/
-        var name = '我是第一个自定义组件哦'
+		/*可以在 render 方法里使用 JavaScript 的语法，使用时加上 {} 即可*/
+		var name = '我是第一个自定义组件哦'
 
-        /**
-         * 返回的组件模板
-         */
-        return (
-            <div>
-                <p> {name} </p>
-            </div>
-        );
-    }
+		/**
+        * 返回的组件模板
+        */
+		return (
+			<div>
+				<p> {name} </p>
+			</div>
+		)
+	}
 }
 
-export default FirstComponent;
+export default FirstComponent
--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ import FirstComponent from './components/firstComponent'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<DatePicker />, document.getElementById('antd_1'))
-// var name = "hehheheh"
 ReactDOM.render(<FirstComponent />, document.getElementById('component_placeholder'))
 registerServiceWorker();
