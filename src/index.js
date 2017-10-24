29b35796204bc252ac8663d6cb564ed3ab640cb0
chenweihong <15094846015@163.com>
2017-10-24 22:03:11 +0800

    Use react-router-dom ,add route demo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,59 @@ import './index.css';
 // import registerServiceWorker from './registerServiceWorker'
 // import { DatePicker } from 'antd'
 // import FirstComponent from './components/firstComponent'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const BasicExample = () => (
+	<Router>
+		<div>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+				<li><Link to="/topics">Topics</Link></li>
+			</ul>
+
+			<hr/>
+
+			<Route exact path="/" component={Home}/>
+			<Route exact path="/about" component={About}/>
+			<Route exact path="/topics" component={Topics}/>
+		</div>
+	</Router>
+);
+// 定义 路由所用到的组件
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+);
+const Topics = ({match}) => (
+	<Router>
+		<div>
+			<h2>Topics</h2>
+			<ul>
+				<li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+				<li><Link to={`${match.url}/components`}>components</Link></li>
+				<li><Link to={`${match.url}/props-v-state`}>Rendering with React</Link></li>
+			</ul>
+			<Route path={`${match.url}/:topicId`} component={Topic}/>
+			<Route exact path={match.url} render={() => (
+				<h3>Please select a topic.</h3>
+			)}/>
+		</div>
+	</Router>
+);
+const Topic = ({ match }) => (
+	<div>
+	  <h3>{match.params.topicId}</h3>
+	</div>
+);
+ReactDOM.render(<BasicExample/>, document.getElementById('antd_1'));
+
 
 // let 块作用域
 console.info(React)
